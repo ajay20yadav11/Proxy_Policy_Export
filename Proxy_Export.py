58f00297a465823c5286b30113ab1e8abb4660fb
Ajay Yadav <ajay20yadav11@gmail.com>
2022-09-17 10:20:24 -0400

    Reformatted using flake8 and black

--- Proxy_Export.py
+++ Proxy_Export.py
@@ -14,10 +14,10 @@ import pandas as pd
 import xlsxwriter
 
 
-print('Start of Python Script')
-print('*' * 100)
+print("Start of Python Script")
+print("*" * 100)
 
-filepath = input('Enter the file name: ')
+filepath = input("Enter the file name: ")
 
 my_xml = open(filepath, "r").read()
 
@@ -27,11 +27,28 @@ new_data = json.dumps(xmltodict.parse(my_xml))
 data = json.loads(new_data)
 
 
-new_line = [ anim for anim in data["config"]["wga_config"]["prox_acl_custom_categories"]["prox_acl_custom_category"] ]
+new_line = [
+    anim
+    for anim in data["config"]["wga_config"]["prox_acl_custom_categories"][
+        "prox_acl_custom_category"
+    ]
+]
 
-url_with_only_url_name, url_with_only_regex_name, url_with_both_url_name, url_with_both_url_name_url, url_with_both_url_name_regex = {},{},{},{},{}
+(
+    url_with_only_url_name,
+    url_with_only_regex_name,
+    url_with_both_url_name,
+    url_with_both_url_name_url,
+    url_with_both_url_name_regex,
+) = ({}, {}, {}, {}, {})
 
-df_with_actual_regex, df_with_both_url, df_with_actual_url, df_with_regex_name, df_with_url_name = [],[],[],[],[]
+(
+    df_with_actual_regex,
+    df_with_both_url,
+    df_with_actual_url,
+    df_with_regex_name,
+    df_with_url_name,
+) = ([], [], [], [], [])
 
 NoneType = type(None)
 
@@ -41,16 +58,12 @@ for line in new_line:
         type(line["prox_acl_custom_category_servers"]) is dict
         and type(line["prox_acl_custom_category_regex_list"]) is dict
     ):
-        url_with_both_url_name_url[
-            line["prox_acl_custom_category_name"]
-        ] = line["prox_acl_custom_category_servers"][
-            "prox_acl_custom_category_server"
-        ]
-        url_with_both_url_name_regex[
-            line["prox_acl_custom_category_name"]
-        ] = line["prox_acl_custom_category_regex_list"][
-            "prox_acl_custom_category_regex"
-        ]
+        url_with_both_url_name_url[line["prox_acl_custom_category_name"]] = line[
+            "prox_acl_custom_category_servers"
+        ]["prox_acl_custom_category_server"]
+        url_with_both_url_name_regex[line["prox_acl_custom_category_name"]] = line[
+            "prox_acl_custom_category_regex_list"
+        ]["prox_acl_custom_category_regex"]
     elif (
         type(line["prox_acl_custom_category_servers"]) is dict
         and type(line["prox_acl_custom_category_regex_list"]) is NoneType
@@ -67,7 +80,6 @@ for line in new_line:
         ]["prox_acl_custom_category_regex"]
 
 
-
 for key, value in url_with_only_url_name.items():
     df_with_url_name.append(key)
     df_with_actual_url.append(value)
@@ -77,40 +89,41 @@ for key, value in url_with_only_regex_name.items():
     df_with_actual_regex.append(value)
 
 
-ip_in_line_data = [ anim for anim in data["config"]["wga_config"]["prox_acl_policy_groups"]["prox_acl_group"] ]
+ip_in_line_data = [
+    anim
+    for anim in data["config"]["wga_config"]["prox_acl_policy_groups"]["prox_acl_group"]
+]
 
 policy_name = []
 
-only_ip = [ anim for anim in ip_in_line_data if "prox_acl_group_ips" in anim ]
+only_ip = [anim for anim in ip_in_line_data if "prox_acl_group_ips" in anim]
 
 policy_map = {}
 
 for line in only_ip:
     if line["prox_acl_group_ips"]:
-        policy_map[line["prox_acl_group_id"]] = line[
-            "prox_acl_group_ips"
-        ]["prox_acl_group_ip"]
+        policy_map[line["prox_acl_group_id"]] = line["prox_acl_group_ips"][
+            "prox_acl_group_ip"
+        ]
     else:
         policy_map[line["prox_acl_group_id"]] = "No IP Configured"
 
 
 copy_to_workbook = xlsxwriter.Workbook("Proxy_Rule.xlsx")
-dark_blue_header_format = copy_to_workbook.add_format({
-    'bg_color': '#00CC66',
-    'font_size': 15,
-    'bold': True, 
-    'border': 1,
-})
+dark_blue_header_format = copy_to_workbook.add_format(
+    {
+        "bg_color": "#00CC66",
+        "font_size": 15,
+        "bold": True,
+        "border": 1,
+    }
+)
 
 
-full_border = copy_to_workbook.add_format({
-    'border': 1
-    
-})
+full_border = copy_to_workbook.add_format({"border": 1})
 outsheet = copy_to_workbook.add_worksheet(name="POLICY name with IP")
 outsheet.write("A1", "Policy Name", dark_blue_header_format)
 outsheet.write("B1", "IP Addresses", dark_blue_header_format)
-
 
 
 count = 1
@@ -121,9 +134,7 @@ for key, value in policy_map.items():
     outsheet.write(count, 1, str(value), full_border)
     count = count + 1
 
-outsheet2 = copy_to_workbook.add_worksheet(
-    name="URL Categories with URL allowed"
-)
+outsheet2 = copy_to_workbook.add_worksheet(name="URL Categories with URL allowed")
 outsheet2.write("A1", "URL Category Name", dark_blue_header_format)
 outsheet2.write("B1", "URL", dark_blue_header_format)
 
@@ -162,8 +173,8 @@ for key, value in url_with_both_url_name_regex.items():
             final_monitor = final_monitor + 1
             break
 
-        
+
 copy_to_workbook.close()
 
-print('*' * 100)
-print('End of Python Script')
+print("*" * 100)
+print("End of Python Script")
